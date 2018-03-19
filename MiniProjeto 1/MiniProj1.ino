6fc532db8ddb5a46841ee60fa25e978e8acbb30f
Victor Meira Pinto <victormeirapinto@icloud.com>
2018-03-19 12:54:23 +0000

    Added Alarm and Alarm changes

--- MiniProjeto 1/MiniProj1.ino
+++ MiniProjeto 1/MiniProj1.ino
@@ -3,13 +3,19 @@
 #define ONE_MINUTE 6000
 
 unsigned long last_clock_update = 0;
-int displayed_time = 1200;
 int clock_time = 1200;
 int alarm_time = 1159;
-int alm_min = 0, alm_hr = 12;
+
+/* Declares what mode the alarm is, 0 is off, 1 is on and waiting, 
+2 is on and playing, 3 is on and turned by user*/
+short alarm_mode = 0;
 
 bool but1_prev_state = 0, but2_prev_state = 0, but3_prev_state = 0;
 unsigned long but1_prev_press = 0, but2_prev_press = 0, but3_prev_press = 0;
+
+/*zero for clock 1 for alarm being displayed */
+bool time_to_be_displayed = 0;
+int displayed_time = 1200;
 
 /* Segment byte maps for numbers 0 to 9 */
 const byte DISPLAY_MAP[] = {0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0X80,0X90};
@@ -81,38 +87,105 @@ void loop(void){
 	bool but2_state = !digitalRead(KEY2);
 	bool but3_state = !digitalRead(KEY3);
 
+	/* if pressing only button 3, change display to alarm*/
+	if(but3_state && !but2_state && !but1_state)
+		time_to_be_displayed = 1;
+
+	/* if alarm being displayed but button 3 not pressed, show clock */
+	if(time_to_be_displayed && !but3_state)
+		time_to_be_displayed = 0;
+
+
 	/* button 2 state changed*/
 	if(but2_state != but2_prev_state){
 		but2_prev_state = but2_state;
 
-		/* button 2 was released while button 1 is pressed */
-		if(but2_state == 0 && but1_state == 1){
-			displayed_time = AddHrToTime(displayed_time);
-		}
-		/*button 2 was released while button 3 is pressed */
-		else{
-			if(but2_state == 0 && but3_state == 1){
-
+		/* When showing the clock */
+		if(time_to_be_displayed == 0){
+			/* button 2 was released while button 1 is pressed */
+			if(but2_state == 0 && but1_state == 1){
+				clock_time = AddHrToTime(clock_time);
 			}
 		}
+		/* When showing the alarm */
+		else{
+			/* Button 2 was released */
+			if(but2_state == 0)
+				alarm_time = AddMinToTime(alarm_time);
+		}	
+
 	}
 
 	/* button 3 state changed*/
 	if(but3_state != but3_prev_state){
 		but3_prev_state = but3_state;
 
-		/* button 3 was released while button 1 is pressed */
-		if(but3_state == 0 && but1_state == 1){
-			displayed_time = AddMinToTime(displayed_time);
+		/* When showing the clock */
+		if(time_to_be_displayed == 0){
+			/* button 3 was released while button 1 is pressed */
+			if(but3_state == 0 && but1_state == 1){
+				clock_time = AddMinToTime(clock_time);
+			}
+			/* button 3 released while no other button is pressed */
+			else{
+				/* Turn alarm from off to on */
+				if(alarm_mode == 0){
+					alarm_mode = 1;
+					digitalWrite(LED1,LOW);
+				}
+				else{
+					/* Turn alarm from on to off */
+					if(alarm_mode == 1){
+						alarm_mode = 0;
+						digitalWrite(LED1,HIGH);
+					}
+					else{
+						/* Stop alarm from playing */
+						if(alarm_mode == 2)
+							alarm_mode = 3;
+					}
+				}
+			}
 		}
-		
+	}
+
+	/* button 1 state changed */
+	if(but1_state != but1_prev_state){
+		but1_prev_state = but1_state;
+
+		/* When showing the alarm */
+		if(time_to_be_displayed){
+			if(but1_state == 0)
+				alarm_time = AddHrToTime(alarm_time);
+		}
 	}
 
 
 	/* one minute has elapsed since last update */
 	if(timer_now - last_clock_update > 100){
-		displayed_time = AddMinToTime(displayed_time);
+		clock_time = AddMinToTime(clock_time);
 		last_clock_update = timer_now;
+	}
+
+	/* what time to be displayed*/
+	if(time_to_be_displayed == 0)
+		displayed_time = clock_time
+	else
+		displayed_time = alarm_time
+
+	/* Alarm is on and the time has arrived*/
+	if(clock_time == alarm_time && alarm_mode == 1)
+		alarm_mode = 2;
+	
+	/* Resets alarm that has stopped playing */
+	if(clock_time != alarm_time && alarm_mode == 3)
+		alarm_mode = 1;
+
+	/* Alarm is playing */
+	if(alarm_mode == 2){
+		digitalWrite(LED4,LOW);
+		delay(20);
+		digitalWrite(LED4,HIGH);
 	}
 
 	WriteNumberInDisplay(displayed_time);
