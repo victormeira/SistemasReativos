554b03a369b09becb19cbfaafdfcad61c2168ac1
Victor Meira Pinto <victormeirapinto@icloud.com>
2018-05-22 19:01:06 -0300

    Update nodeMCUclient.lua

--- NodeMCU/nodeMCUclient.lua
+++ NodeMCU/nodeMCUclient.lua
@@ -10,40 +10,45 @@ gpio.write  (led2, gpio.LOW)
 locationJSON = ""   
 
 
-local m = mqtt.Client("MiniProjeto-MatheusEVictor", 120)
+local m = mqtt.Client("1420626-11", 120)
 
 function publishLocationData()
-	if(locationJSON != "") then
+	if(string.len(locationJSON) > 0) then
 		print("Publishing Location Data.")
-		--publish na fila locationData
+		client:publish("locationData", "1;2;3",0,  0, 
+                  function(client, reason) print("Published.") end)
 		print("Publish successful.")
-	end
 	else
 		print("Publish failed. Data was empty")
 	end 
 end
 
 --topic te diz a fila que recebeu e message a string
-function messageReceivedCallback(topic, message)
-  if(message == "Requesting location") then
-	  locationJSON = ""
-	  wifi.sta.getap(listap)
-	  publishLocationData()
-  end     
+function messageReceivedCallback(client)
+
+    local function messageTreatment(userdata, topic, message)
+        print("ENTREI")
+            if(message == "A") then
+                locationJSON = ""
+                wifi.sta.getap(listap)
+            end
+    end
+
+    client:on("message",messageTreatment)
 end
 
 function connected(client)
-  client:publish("connectionSuccess", "NodeMCU has connected.",0,0, 
-			  	  function(client, reason) print("Connection failed. Reason for failure:"..reason) end)
-  client:subscribe("requests",0,messageReceivedCallback)
+    --client:publish("connectionSuccess", "NodeMCU has connected.",0,  0, 
+	--		  	  function(client, reason) print("Published.") end)
+    client:subscribe("requestData",0,messageReceivedCallback)
 end
 
 function parseLocationData(message)
 	local startLat,endLat = string.find(message,"lat")
-	local startLoc,endLoc = string.find(message,"loc")
+	local startLng,endLng = string.find(message,"lng")
 	local startAcc,endAcc = string.find(message,"accuracy")
 
-	return string.sub(message,endLat + 3, startLoc - 4) .. ";" .. string.sub(message,endLoc + 3 , startAcc - 4) .. ";" .. string.sub(message,endAcc-4,string.len(message) - 5)
+	return string.sub(message,endLat + 4, startLng - 6) .. ";" .. string.sub(message,endLng + 4 , startAcc - 8) .. ";" .. string.sub(message,endAcc+4,string.len(message) - 3)
 end
 
 -- Print AP list that is easier to read
@@ -55,15 +60,16 @@ function listap(t) -- (SSID : Authmode, RSSI, BSSID, Channel)
 	for ssid,v in pairs(t) do
 		local authmode, rssi, bssid, channel = string.match(v, "([^,]+),([^,]+),([^,]+),([^,]+)")
 
-		listdeap[i] = "{ \"macAddress\": \"" .. bssid .. "\"" .. ", \"signalStrength\": " .. rssi .. ", \"channel\": " .. channel .. "}"
+		listdeap[i] = "\n\t\t{ \"macAddress\": \"" .. bssid .. "\"" .. ",\n\t\t  \"signalStrength\": " .. rssi .. ",\n\t\t  \"channel\": " .. channel .. "}"
 
+        if (i == 4) then break end
 		i = i + 1
 	end
 
-	json = "[[{ \"wifiAccessPoints\": ["
+	json = "[[\n{\n\t\"wifiAccessPoints\": ["
 	json = json .. table.concat(listdeap,",")
 
-	json = json .. "]}]]"
+	json = json .. "\n\t]\n}\n]]"
 	
 	print(json)
 	
@@ -71,18 +77,30 @@ function listap(t) -- (SSID : Authmode, RSSI, BSSID, Channel)
 	for k in pairs (listdeap) do
 		listdeap [k] = nil
 	end
-	
-	http.post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyA5Z5xai0SkBDbbjHueWLggvXvV_r', --LMG5E
-  'Content-Type: application/json\r\n',json,
-  	function(code, data)
-		if (code < 0) then
-	  		print("HTTP request failed :", code)
-		else
-	  		print(code, data)
-	  		locationJSON = parseLocationData(data)       
-		end
-  	end)
- 
+
+    local numberOfTries = 20
+
+    local function callbackPost(code,data)
+            if (code < 0) then
+                print("HTTP request failed :", code)
+                numberOfTries = numberOfTries - 1
+                print("Number of remaining tries: " .. numberOfTries)
+                if(numberOfTries > 0) then
+                   http.post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDKcCPg4oxcRCVu-sYs97V1VHNYCdVKn_o',
+                    'Content-Type: application/json\r\n',json,callbackPost)
+                end
+            else
+                print(code, data)
+                locationJSON = parseLocationData(data)
+                print(locationJSON)      
+            end
+    end
+
+    http.post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDKcCPg4oxcRCVu-sYs97V1VHNYCdVKn_o',
+      'Content-Type: application/json\r\n',json,callbackPost)
+        
+    gpio.write  (led1, gpio.LOW)
+    publishLocationData()
 
 end
 
@@ -103,9 +121,9 @@ function buttonpressed ()
 
 			gpio.write  (led1, gpio.HIGH)
 			wifi.sta.getap(listap)
-			gpio.write  (led1, gpio.LOW)
+			
 
-			publishLocationData()
+			--publishLocationData()
 
 	end
 end
