e6b0048cb43c04fd71e382e18e93e471a1ac1389
DESKTOP-V4BVPHL\Matheus Cunha <matheuspenso96@gmail.com>
2018-05-22 19:01:18 -0300

    aula 2205

--- NodeMCU/Love/main.lua
+++ NodeMCU/Love/main.lua
@@ -1,4 +1,7 @@
 local mqtt = require("mqtt_library")
+local latitude = 0
+local longitude = 0
+local precisao = 0
 
 function split(s, delimiter)
     result = {};
@@ -8,29 +11,17 @@ function split(s, delimiter)
     return result;
 end
 
-function print_list_list(list)
-
-for i = 1, #list, 1 do
-  for j = 1, #list, 1 do
-    print(list[i][j])
-  end
-end
-
-end
 
 function split_message(message)
   
   local list_1 = {}
-  local list_2 = {{}}
 
   --Splita a string message pelo ;
   list_1 = split(message, ";")
   
-  for i = 1, #list_1, 1 do
-    list_2[i] = split(list_1[i], ",")
-  end
-  
-  print_list_list(list_2)
+  latitude = list_1[1]
+  longitude = list_1[2]
+  precisao = list_1[3]
   
 end
 
@@ -42,24 +33,35 @@ end
 
 function love.keypressed(key)
   if key == 'a' then
-    mqtt_client:publish("requestData", "Requesting location")
+    latitude = 10
+    mqtt_client:publish("requestData", "A")
   end
 end
 
 function love.load()
+  
+  WIDTH = 500
+  HEIGHT = 500
+  
+  love.window.setMode(WIDTH,HEIGHT)
 
   mqtt_client = mqtt.client.create("test.mosquitto.org", 1883, mqttcb)
-  mqtt_client:connect("MiniProjeto-MatheusEVictor")
+  mqtt_client:connect("MiniProjeto-MatheusEVictor-Love8")
   mqtt_client:subscribe({"locationData"})
+  
+  
 end
 
 function love.draw()
-
+  love.graphics.print("Latitude: ", 10,10)
+  love.graphics.print(latitude, 80,10)
+  love.graphics.print("Longitude: ", 10,30)
+  love.graphics.print(longitude, 80,30)
+  love.graphics.print("Precisão: ", 10,50)
+  love.graphics.print(precisao, 80,50)
 end
 
 function love.update(dt)
   mqtt_client:handler()
 end
 
-
---split_message("a:1,a:2,a:3;b:1,b:2,b:3;c:1,c:2,c:3;d:1,d:2,d:3")
